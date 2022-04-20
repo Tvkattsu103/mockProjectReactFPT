27892e180d315885682957d56333f4f658500e1d
Tuan <voanhtuan103@gmail.com>
2022-04-20 21:26:53 +0700

    add list category

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import CarouselUI from './components/CarouselUI';
 import Category from './components/Category';
 
 function App() {
   return (
-    <Category/>
+    // <Category/>
+    <CarouselUI/>
   );
 }
 
--- src/components/Category.js
+++ src/components/Category.js
@@ -2,19 +2,88 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Box, Grid, List, ListItem, Stack, ListItemButton, ListItemText } from '@mui/material';
+import { Box, Grid, List, ListItem, Stack, ListItemButton, ListItemText, imageListItemClasses } from '@mui/material';
 
 function Category() {
+    const category = [
+        {
+            name: 'New Arrivals',
+            img: 'https://www.gap.com/Asset_Archive/GPWeb/content/0028/619/791/assets/NEW_ARRIVALS.jpg'
+        },
+        {
+            name: 'Jeans',
+            img: 'https://www.gap.com/Asset_Archive/GPWeb/content/0028/619/791/assets/JEANS.jpg'
+        },
+        {
+            name: 'Pants',
+            img: 'https://www.gap.com/Asset_Archive/GPWeb/content/0028/619/791/assets/PANTS.jpg'
+        },
+        {
+            name: 'Sweats',
+            img: 'https://www.gap.com/Asset_Archive/GPWeb/content/0028/619/791/assets/SWEATS.jpg'
+        },
+        {
+            name: 'Sweaters',
+            img: 'https://www.gap.com/Asset_Archive/GPWeb/content/0028/619/791/assets/SWEATERS.jpg'
+        },
+        {
+            name: 'Dresses',
+            img: 'https://www.gap.com/Asset_Archive/GPWeb/content/0028/619/791/assets/DRESSES.jpg'
+        },
+        {
+            name: 'Outerwear',
+            img: 'https://www.gap.com/Asset_Archive/GPWeb/content/0028/619/791/assets/OUTERWEAR.jpg'
+        },
+        {
+            name: 'Tees',
+            img: 'https://www.gap.com/Asset_Archive/GPWeb/content/0028/619/791/assets/TEES.jpg'
+        }
+    ]
+    const listcategory2 = [
+        'New Arrivals', 'Jeans', 'Dresses', 'Jumpsuits & Rompers', 'T-shirts', 'Shirts & Tops', 'Sweatshirts & Sweatpants',
+        'Sweaters', 'Outerwear & Jacket', 'Pants', 'Shorts', 'Skirts', 'Swim', 'Shoes & Accessories', 'Gapfit',
+        'Gapbody', 'Sale'
+    ]
+    const data = [
+        {
+            name: "Pizza Begin",
+            link: "pizza-begin.co.il",
+        },
+        {
+            name: "Mia Luz",
+            link: "mia-luz.com",
+        },
+        {
+            name: "Nuda Swim",
+            link: "nudaswim.com"
+        }
+       ];
+    // const sliderItems = data.length > 3 ? 3 : data.length;
+    // const items = [];
+
+    // for (let i = 0; i < data.length; i += sliderItems) {
+    //     if (i % sliderItems === 0) {
+    //         items.push(
+    //             <Card raised className="Banner" key={i.toString()}>
+    //                 <Grid container spacing={0} className="BannerGrid">
+    //                     {data.slice(i, i + sliderItems).map((da, index) => {
+    //                         return <SubCategory key={index.toString()} item={da} />;
+    //                     })}
+    //                 </Grid>
+    //             </Card>
+    //         );
+    //     }
+    // }
     return (
         <>
             <Grid container className="category-container">
-                <Grid container xs={12}>
+                {category.map(category => (
                     <Grid item xs={3}>
                         <Card style={{ width: '100%' }} className="Card">
                             <Box sx={{ position: 'relative' }}>
                                 <CardMedia
                                     component="img"
-                                    image="https://www.gap.com/Asset_Archive/GPWeb/content/0028/619/791/assets/NEW_ARRIVALS.jpg"
+                                    image={category.img}
                                     alt="Paella dish"
                                 />
                                 <Box className="Box"
@@ -27,139 +96,62 @@ function Category() {
                                         color: 'white',
                                     }}
                                 >
-                                    <Typography variant="h5">Lizard</Typography>
+                                    <Typography variant="h5" style={{ textTransform: 'uppercase' }}>{category.name}</Typography>
                                 </Box>
                             </Box>
 
                         </Card>
                     </Grid>
-                    <Grid item xs={3}>
-                        <Card style={{ width: '100%' }} className="Card">
-                            <Box sx={{ position: 'relative' }}>
-                                <CardMedia
-                                    component="img"
-                                    image="https://www.gap.com/Asset_Archive/GPWeb/content/0028/619/791/assets/JEANS.jpg"
-                                    alt="Paella dish"
-                                />
-                                <Box className="Box"
-                                    sx={{
-                                        position: 'absolute',
-                                        left: '50%',
-                                        bottom: '50%',
-                                        transform: 'translateX(-50%)',
-                                        bgcolor: 'transparent',
-                                        color: 'white'
-                                    }}
-                                >
-                                    <Typography variant="h5">Animal Store life</Typography>
-                                </Box>
-                            </Box>
-
-                        </Card>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Card style={{ width: '100%' }} className="Card">
-                            <Box sx={{ position: 'relative' }}>
-                                <CardMedia
-                                    component="img"
-                                    image="https://www.gap.com/Asset_Archive/GPWeb/content/0028/619/791/assets/PANTS.jpg"
-                                    alt="Paella dish"
-                                />
-                                <Box className="Box"
-                                    sx={{
-                                        position: 'absolute',
-                                        left: '50%',
-                                        bottom: '50%',
-                                        transform: 'translateX(-50%)',
-                                        bgcolor: 'transparent',
-                                        color: 'white'
-                                    }}
-                                >
-                                    <Typography variant="h5">Animal Store life</Typography>
-                                </Box>
-                            </Box>
-
-                        </Card>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Card style={{ width: '100%' }} className="Card">
-                            <Box sx={{ position: 'relative' }}>
-                                <CardMedia
-                                    component="img"
-                                    image="https://www.gap.com/Asset_Archive/GPWeb/content/0028/619/791/assets/SWEATS.jpg"
-                                    alt="Paella dish"
-                                />
-                                <Box className="Box"
-                                    sx={{
-                                        position: 'absolute',
-                                        left: '50%',
-                                        bottom: '50%',
-                                        transform: 'translateX(-50%)',
-                                        bgcolor: 'transparent',
-                                        color: 'white'
-                                    }}
-                                >
-                                    <Typography variant="h5">Animal Store life</Typography>
-                                </Box>
-                            </Box>
-
-                        </Card>
-                    </Grid>
-                </Grid>
-                {/* <Grid item xs={0.5} style={{ backgroundColor: 'red' }}></Grid> */}
+                ))}
             </Grid>
             <Grid
-  container
-  spacing={0}
-  direction="column"
-  alignItems="center"
-  justifyContent="center"
->
-
-  <Grid item xs={3}>
-  <List component={Stack} direction="row" >
-                    <ListItem disablePadding style={{width: 'auto', minWidth: '0'}}>
-                        <ListItemButton>
-                            <ListItemText primary="Inbox"/>
-                        </ListItemButton>
-                        <ListItemButton>
-
-                            <ListItemText primary="Inbox"/>
-                        </ListItemButton>
-                        <ListItemButton>
-
-                            <ListItemText primary="Inbox"/>
-                        </ListItemButton>
-                        <ListItemButton>
-
-                            <ListItemText primary="Inbox"/>
-                        </ListItemButton>
-                    </ListItem>
-                </List>
-  </Grid>   
-   
-</Grid> 
-            {/* <Box sx={{bgcolor: 'background.paper', width: '350px', maxWidth: '550px', margin:'auto' }}>
-                <List component={Stack} direction="row" >
-                    <ListItem disablePadding style={{width: 'auto', minWidth: '0'}}>
-                        <ListItemButton>
-                            <ListItemText primary="Inbox"/>
-                        </ListItemButton>
-                        <ListItemButton>
-
-                            <ListItemText primary="Inbox"/>
-                        </ListItemButton>
-                        <ListItemButton>
-
-                            <ListItemText primary="Inbox"/>
-                        </ListItemButton>
-                        <ListItemButton>
-
-                            <ListItemText primary="Inbox"/>
-                        </ListItemButton>
-                    </ListItem>
-                </List>
-            </Box> */}
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+            >
+                <Grid item xs={3} >
+                    <List component={Stack} direction="row" style={{ padding: '0 7px' }}>
+                        <ListItem disablePadding >
+                            {listcategory2.filter((item, index) => index <= 6).map(listcate => (
+                                <ListItemButton style={{ padding: '0' }}>
+                                    <ListItemText primary={listcate} style={{ padding: '0 12px' }} className="list-category" />
+                                </ListItemButton>
+                            ))}
+                        </ListItem>
+                    </List>
+                </Grid>
+                <Grid item xs={3} >
+                    <List component={Stack} direction="row" style={{ padding: '0 7px' }}>
+                        <ListItem disablePadding >
+                            {listcategory2.filter((item, index) => index > 6).map(listcate => (
+                                <ListItemButton style={{ padding: '0' }}>
+                                    <ListItemText primary={listcate} style={{ padding: '0 12px' }} className="list-category" />
+                                </ListItemButton>
+                            ))}
+                        </ListItem>
+                    </List>
+                </Grid>
+            </Grid>
+            <Grid
+                container
+                spacing={0}
+                marginTop={7}
+            >
+                <Grid item xs={1.5} />
+                <Grid container xs={9} >
+                    <Grid item xs={12}>
+                        <Typography hy variant="h5">We Know You’ll Love These</Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        {/* <Carousel animation="slide" autoPlay={false} cycleNavigation timeout={300}>
+                            {items}
+                        </Carousel> */}
+                    </Grid>
+                </Grid>
+                <Grid item xs={1.5} />
+            </Grid>
         </>
     );
 }
