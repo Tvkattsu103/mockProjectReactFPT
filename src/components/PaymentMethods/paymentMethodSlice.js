5797ad620ef7307a84002d77d826e9323295badc
Phuong <hvp230499@gmail.com>
2022-05-06 11:35:01 +0700

    update addMethod

--- src/components/PaymentMethods/paymentMethodSlice.js
+++ src/components/PaymentMethods/paymentMethodSlice.js
@@ -30,90 +30,107 @@ export default createSlice({
     //   isDefault: false,
     // },
     {
-      "id": 1,
-      "method": "diners-club-international",
-      "expMonth": 3,
-      "expYear": 23,
-      "customerName": "Ambrose Fullwood",
+      id: 1,
+      method: "diners-club-international",
+      expMonth: 3,
+      expYear: 23,
+      customerName: "Ambrose Fullwood",
       image: require("../../images/visaIcon.png"),
-      "isDefault": true
-    }, {
-      "id": 2,
-      "method": "laser",
-      "expMonth": 6,
-      "expYear": 24,
-      "customerName": "Donny Monks",
+      isDefault: true,
+    },
+    {
+      id: 2,
+      method: "laser",
+      expMonth: 6,
+      expYear: 24,
+      customerName: "Donny Monks",
       image: require("../../images/visaIcon.png"),
-      "isDefault": false
-    }, {
-      "id": 3,
-      "method": "americanexpress",
-      "expMonth": 9,
-      "expYear": 23,
-      "customerName": "Jamie Trevascus",
+      isDefault: false,
+    },
+    {
+      id: 3,
+      method: "americanexpress",
+      expMonth: 9,
+      expYear: 23,
+      customerName: "Jamie Trevascus",
       image: require("../../images/visaIcon.png"),
-      "isDefault": false
-    }, {
-      "id": 4,
-      "method": "jcb",
-      "expMonth": 10,
-      "expYear": 24,
-      "customerName": "Dimitri Clack",
+      isDefault: false,
+    },
+    {
+      id: 4,
+      method: "jcb",
+      expMonth: 10,
+      expYear: 24,
+      customerName: "Dimitri Clack",
       image: require("../../images/visaIcon.png"),
-      "isDefault": false
-    }, {
-      "id": 5,
-      "method": "jcb",
-      "expMonth": 7,
-      "expYear": 23,
-      "customerName": "Niccolo Hakey",
+      isDefault: false,
+    },
+    {
+      id: 5,
+      method: "jcb",
+      expMonth: 7,
+      expYear: 23,
+      customerName: "Niccolo Hakey",
       image: require("../../images/visaIcon.png"),
-      "isDefault": false
-    }, {
-      "id": 6,
-      "method": "jcb",
-      "expMonth": 1,
-      "expYear": 22,
-      "customerName": "Gris Coleman",
+      isDefault: false,
+    },
+    {
+      id: 6,
+      method: "jcb",
+      expMonth: 1,
+      expYear: 22,
+      customerName: "Gris Coleman",
       image: require("../../images/visaIcon.png"),
-      "isDefault": false
-    }, {
-      "id": 7,
-      "method": "diners-club-enroute",
-      "expMonth": 1,
-      "expYear": 21,
-      "customerName": "Ariella Gouck",
+      isDefault: false,
+    },
+    {
+      id: 7,
+      method: "diners-club-enroute",
+      expMonth: 1,
+      expYear: 21,
+      customerName: "Ariella Gouck",
       image: require("../../images/visaIcon.png"),
-      "isDefault": false
-    }, {
-      "id": 8,
-      "method": "maestro",
-      "expMonth": 6,
-      "expYear": 20,
-      "customerName": "Michaela Urion",
+      isDefault: false,
+    },
+    {
+      id: 8,
+      method: "maestro",
+      expMonth: 6,
+      expYear: 20,
+      customerName: "Michaela Urion",
       image: require("../../images/visaIcon.png"),
-      "isDefault": false
-    }, {
-      "id": 9,
-      "method": "china-unionpay",
-      "expMonth": 3,
-      "expYear": 20,
-      "customerName": "Farlie Goulden",
+      isDefault: false,
+    },
+    {
+      id: 9,
+      method: "china-unionpay",
+      expMonth: 3,
+      expYear: 20,
+      customerName: "Farlie Goulden",
       image: require("../../images/visaIcon.png"),
-      "isDefault": false
-    }, {
-      "id": 10,
-      "method": "jcb",
-      "expMonth": 11,
-      "expYear": 21,
-      "customerName": "Georgeanna Storre",
+      isDefault: false,
+    },
+    {
+      id: 10,
+      method: "jcb",
+      expMonth: 11,
+      expYear: 21,
+      customerName: "Georgeanna Storre",
       image: require("../../images/visaIcon.png"),
-      "isDefault": false
-    }]
-    ,
+      isDefault: false,
+    },
+  ],
   reducers: {
     addMethod: (state, action) => {
-      state.push(action.payload);
+      state.push({
+        id: state.length + 1,
+        method: action.payload.type,
+        expMonth: action.payload.month,
+        expYear: action.payload.year,
+        customerName: action.payload.name,
+        image: require("../../images/visaIcon.png"),
+        isDefault: false,
+      });
     },
     deleteMethod: (state, action) => {
       const index = state.findIndex((m) => m.id === action.payload);
