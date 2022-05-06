8d8114d2c918fa19b3882d80a0b3bc71c5273f8e
Phuong <hvp230499@gmail.com>
2022-05-06 11:38:29 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
 import './App.css';
-import Category from './components/Category';
+import Category from './components/CategoryOverview/Category';
+import Help from './components/Help/Help';
+import "bootstrap-icons/font/bootstrap-icons.css";
+import HomePage from "./components/Home/HomePage";
+import OrderConfirmation from "./components/OrderConfirmation/OrderConfirmationPage";
+import PaymentMethods from "./components/PaymentMethods/PaymentMethods";
+import Search from "./components/Search/Search";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Payment from "./components/Payment/Payment";
+import EmailSignUp from "./components/EmailSignUp/EmailSignUp";
+import Address from "./components/Address/Address";
+import Login from "./components/Login/Login";
+import OrderHistory from "./components/Order/OrderHistory";
+import Register from "./components/Register/Register";
+import Shipping from "./components/Shipping/Shipping";
+
+import CardPage from './components/UICartPage/CardPage';
+import UIMyProfile from './components/UIMyProfile/Main';
+import Product from './components/UIProduct/Product.js';
+import MainReturn from './components/UIReturns/MainReturn';
 
 function App() {
   return (
-    <Category/>
+    <>
+      <Router>
+        <div id="main-ct" className="p-child-dkr">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/category" element={<Category />} />
+            <Route path="/payment" element={<Payment />} />
+            <Route path="/emailsignup" element={<EmailSignUp />} />
+            {/* <Route path="/" element={<><HomePage /><Outlet /></>} /> */}
+            {/* <Route path="HomePage" element={<HomePage />} /> */}
+            <Route path="/Search" element={<Search />} />
+            <Route path="/PaymentMethods" element={<PaymentMethods />} />
+            {/* <Route path="/" exact element={<App/>}></Route> */}
+            <Route path="/Login" element={<Login />}></Route>
+            <Route path="/Register" element={<Register />}></Route>
+            <Route path="/Shipping" element={<Shipping />}></Route>
+            <Route path="/OrderHistory" element={<OrderHistory />}></Route>
+            <Route path="/Address" element={<Address />}></Route>
+
+            <Route path="CardPage" element={<CardPage />} />
+            <Route path="Product" element={<Product />} />
+            <Route path="MyProfile" element={<UIMyProfile />} />
+            <Route path="/CheckoutReview" element={<OrderConfirmation />} />
+            <Route path="MainReturn" element={<MainReturn />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
